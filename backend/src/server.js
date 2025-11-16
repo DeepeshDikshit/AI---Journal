f2e03a8622c912c5b00420736f46845190d52854
Deepesh Sharma <dikshitdipesh@gmail.com>
2025-11-16 21:20:36 +0530

    dist commit for server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -34,7 +34,7 @@ app.get('/', (req, res) => {
 });
 
 // For React/Frontend Routing
-app.get("*", (req, res) => {
+app.get("*name", (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
