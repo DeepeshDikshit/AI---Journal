91f129e825ef31119730aa3593ee9418055f7e23
Deepesh Sharma <dikshitdipesh@gmail.com>
2025-11-16 15:18:18 +0530

    file path

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import cors from 'cors'
 import dotenv from 'dotenv'
 import connectDB from "./db/db.js";
-import authRoutes from "./routes/authroutes.js";
+import authRoutes from "./routes/authRoutes.js";
 import journalRoutes from "./routes/journalRoutes.js"
 
 
