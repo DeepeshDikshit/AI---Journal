ce8860091c518c0ed0bffde205eaaeff4bc460a0
Deepesh Sharma <dikshitdipesh@gmail.com>
2025-11-16 21:05:31 +0530

    dist commit for server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,8 +5,7 @@ import dotenv from 'dotenv'
 import connectDB from "./db/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import journalRoutes from "./routes/journalRoutes.js"
-
-
+const path = require('path');
 
 
 dotenv.config()
@@ -17,6 +16,7 @@ app.use(cors())
 app.use(express.json());
 app.use('/api/auth',authRoutes)
 app.use("/api/journal",journalRoutes)
+app.use(express.static(path.join(__dirname, '../public')))
 
 //connect to mongodb
 
@@ -25,6 +25,10 @@ connectDB()
 app.get('/',(req,res)=>{
   res.send("API is running....")
 })
+
+app.get("*name", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 //ports where the srevr runs
 app.listen(5000,()=>{
